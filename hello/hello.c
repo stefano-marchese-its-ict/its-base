95b5156ac613d00c859c0ad14972e0e52d895ce8
stefano-marchese-its-ict <stefano.marchese@its-ictpiemonte.it>
2024-12-20 15:44:31 +0000

    created hello.c

---
+++ hello/hello.c
@@ -0,0 +1,15 @@
+// include standard input/output library
+#include <stdio.h>
+
+// main function - the entry point 
+// the return value of the main is an integer
+int main()
+{
+    // print the message "Hello World!!!" to the console
+    // \n is the shortcut for new line
+    printf("Hello World!!!\n\n\n");
+
+    /* return 0: indicated that the program is terminated
+       successfully */
+    return 0;
+}
