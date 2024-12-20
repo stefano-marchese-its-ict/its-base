7f42983cf5a10d8f369b2bc473d346929a470238
stefano-marchese-its-ict <stefano.marchese@its-ictpiemonte.it>
2024-12-20 16:09:15 +0000

    hello.c updated

--- hello/hello.c
+++ hello/hello.c
@@ -9,6 +9,18 @@ int main()
     // \n is the shortcut for new line
     printf("Hello World!!!\n\n\n");
 
+    // variable of 32 chars
+    char name[32];
+
+    // ask for a name
+    printf("Enter a name: ");
+
+    // read the input of the user
+    scanf("%s", name);
+
+    // print a message with the name entered
+    printf("\nHello\t%s\n\n\n", name);
+
     /* return 0: indicated that the program is terminated
        successfully */
     return 0;
