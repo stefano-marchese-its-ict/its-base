ca6035201e50df2310a9d5b06b1bf50cce637712
stefano-marchese-its-ict <stefano.marchese@its-ictpiemonte.it>
2025-01-31 14:38:13 +0100

    Create main.c

---
+++ 05-for/main.c
@@ -0,0 +1,3 @@
+/* legga un numero intero positivo N dall'utente e stampi un triangolo di numeri con N righe
+per tutti i cicli utilizzare il for
+controllare che l'input inserito dall'utente sia positivo; in caso contrario, richiedi di reinserire il valore */
